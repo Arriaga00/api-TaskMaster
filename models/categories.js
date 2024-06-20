903fde9c2f7311eeecb696897095e6192ea99bf6
andres arriaga <andresfelipearriaga@gmail.com>
2024-06-20 12:58:29 -0500

    cambiando nombre de las tablas

--- models/categories.js
+++ models/categories.js
@@ -1,20 +1,26 @@
 import db from "../db/connection.js";
 import { DataTypes } from "sequelize";
 
-const Categories = db.define("Categories", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
+const Categories = db.define(
+  "Categories",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    id_folder: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  id_folder: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+  {
+    tableName: "categories",
+  }
+);
 
 export default Categories;
--- models/folders.js
+++ models/folders.js
@@ -1,20 +1,26 @@
 import db from "../db/connection.js";
 import { DataTypes } from "sequelize";
 
-const Folders = db.define("Folders", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
+const Folders = db.define(
+  "Folders",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    id_user: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  id_user: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-});
+  {
+    tableName: "folders",
+  }
+);
 
 export default Folders;
